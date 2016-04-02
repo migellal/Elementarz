94b45a720788e8e500d5cb0d228e54c0716adcc6
migellal <michalgellert@gmail.com>
2016-04-02 14:35:29 +0200

    Morphing animation added

--- app/src/main/java/org/e_lementarz/elementarz/activities/NumbersOrderActivity.java
+++ app/src/main/java/org/e_lementarz/elementarz/activities/NumbersOrderActivity.java
@@ -1,11 +1,15 @@
 package org.e_lementarz.elementarz.activities;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+
+import com.wnafee.vector.compat.ResourcesCompat;
 
 import org.e_lementarz.elementarz.R;
 import org.e_lementarz.elementarz.common.ElementarzActivity;
@@ -38,7 +42,11 @@ public class NumbersOrderActivity extends ElementarzActivity {
                     bricksArray = stackBricksViewGroup.getBricksStack(bricksStacks[counter], counter);
                     stackBricksViewGroup.refreshAllViewsOnlyAdding(counter, bricksArray, animPut);
                     if(counter==10) {
-                        fab.setImageResource(R.drawable.ic_done_white_48dp);
+                        //fab.setImageResource(R.drawable.ic_done_white_48dp);
+                        final Animatable icon = (Animatable) ResourcesCompat.getDrawable(getApplicationContext(), R.drawable.ic_line_to_done);
+                        fab.setImageDrawable((Drawable)icon);
+                        icon.start();
+                        fab.setBackgroundTintList(getResources().getColorStateList(R.color.colorSuccessGreen));
                     }
                 }
             }
